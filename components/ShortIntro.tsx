2f84773adc345c3c3fa91a09b7d1a25a0fe05133
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-03-18 05:21:18 +0530

    fix: introduction alignment

--- components/ShortIntro.tsx
+++ components/ShortIntro.tsx
@@ -5,11 +5,11 @@ const ShortIntro = () => {
   return (
     <div className="space-y-5 my-5 mt-7 font-medium">
 
-      <p className="text-xl space-x-2 mb-4">
+      <p className="text-xl space-x-2 mb-5">
         <span>Hi, I'm</span>
         <span className={`font-medium text-primary-600`}>Abhijay Rajvansh</span>
       </p>
-      {/* full-stack: className="bg-success-100 bg-opacity-60 px-2 rounded-xl text-success-700" */}
+
       <h1 className={`${title()}text-[2rem] leading-tight font-semibold`}>
         <span>I develop </span>
           <span className={title({color:'green'})}>full-stack</span>
@@ -17,7 +17,7 @@ const ShortIntro = () => {
         </span>
       </h1>
 
-      <p className="font-light text-default-550 leading-relaxed">
+      <p className="pt-3 font-light text-default-550 leading-relaxed">
         <span className="">Freelance</span> Web & Mobile Developer, <span className="">Former Silicon Valley</span> Intern, <span className="">Apple</span> Development Trainee, Former <span className="">AI/ML Researcher</span> @L&D Hospital, UK and <Link className="text-primary-600 font-medium" href={'/about'}>more...</Link>
       </p>
 
