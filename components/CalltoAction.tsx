5adb02f809dc7530e63cb1cc8c1e43eb97b91f8e
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-03-27 11:12:12 +0530

    update: hydration fix

--- components/CalltoAction.tsx
+++ components/CalltoAction.tsx
@@ -4,7 +4,7 @@ import { Link } from "@nextui-org/react";
 
 const CalltoAction = () => {
   return (
-    <p className="text-color-text">
+    <div className="text-color-text">
       Shoot me an{" "}
       <Link href={siteConfig.links.email} className="text-primary-500 font-medium">
         Email
@@ -20,7 +20,7 @@ const CalltoAction = () => {
       , I&apos;m always motivated & interested in
       exploring potential opportunities, having discussions with you, and
       potentially collaborating if it aligns well.
-    </p>
+    </div>
   )
 }
 
