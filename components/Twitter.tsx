8393cafa9bb00a7280e9ec730e977df59ab41c96
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-03-29 12:43:09 +0530

    update: twitter & spotify card ui

--- components/Twitter.tsx
+++ components/Twitter.tsx
@@ -1,4 +1,5 @@
-import { Button, Slider } from "@nextui-org/react";
+import { siteConfig } from "@/config/site";
+import { Button, Link, Slider } from "@nextui-org/react";
 import Image from "next/image";
 
 interface twitterProps {
@@ -10,7 +11,7 @@ const Twitter: React.FC<twitterProps> = ({tilt}) => {
     <div className={`ml-7 mb-7 shadow-xl rounded-xl mr-5 transition ${tilt === 'left' ? 'hover:-rotate-1' : 'hover:rotate-1'}  relative sm:ml-[116px] lg:ml-7 sm:w-[650px] lg:max-w-[300px] flex items-center justify-center border border-default-200/50`}>
     
 
-        <div className="w-full rounded-xl backdrop-blur-2xl flex flex-col items-start space-y-4 p-3 bg-black/90">
+        <div className="w-full rounded-xl backdrop-blur-2xl flex flex-col items-start space-y-3 bg-black/90 p-4">
 
          <div className="flex items-center w-full justify-between">
           <div className="flex items-center gap-3">
@@ -21,7 +22,7 @@ const Twitter: React.FC<twitterProps> = ({tilt}) => {
               <p className="text-color-text text-sm">@rajvanshtwt</p>
             </div>
           </div>
-          <Button size="sm" className="bg-white text-black font-medium rounded-full px-3">Follow</Button>
+          <Button as={Link} isExternal href={siteConfig.links.twitter} size="sm" className="bg-white text-black font-medium rounded-full px-3">Follow</Button>
          </div>
 
          <p className="text-white text-sm"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit voluptatum culpa consectetur, non quaerat minus molestias sit quisquam! </p>
