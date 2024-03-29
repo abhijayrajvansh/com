a4f6ddeb6f00f355d52a95b24360b83be7ae0134
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-03-29 15:52:54 +0530

    update: fixed twitter ui dark, light

--- components/Twitter.tsx
+++ components/Twitter.tsx
@@ -8,10 +8,10 @@ interface twitterProps {
 
 const Twitter: React.FC<twitterProps> = ({ tilt }) => {
   return (
-    <div className={`ml-7 mb-7 shadow-xl rounded-xl mr-5 transition ${tilt === 'left' ? 'hover:-rotate-1' : 'hover:rotate-1'}  relative sm:ml-[116px] lg:ml-7 sm:w-[650px] lg:max-w-[300px] flex items-center justify-center border border-default-200/50`}>
+    <div className={`ml-7 mb-7 shadow-xl rounded-xl mr-5 transition ${tilt === 'left' ? 'hover:-rotate-1' : 'hover:rotate-1'}  relative sm:ml-[116px] lg:ml-7 sm:w-[650px] lg:max-w-[300px] flex items-center justify-center border border-default-200/50 bg-gradient-to-b from-projectCardTop to-projectCardBotttom`}>
     
 
-        <div className="w-full rounded-xl backdrop-blur-2xl flex flex-col items-start space-y-3 bg-black/90 p-4">
+        <div className="w-full rounded-xl backdrop-blur-2xl flex flex-col items-start space-y-3 p-4 dark:bg-black/30 bg-black/90">
 
          <div className="flex items-center w-full justify-between">
           <div className="flex items-center gap-3">
