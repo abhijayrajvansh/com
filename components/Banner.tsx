714bce14712914ae557f095cedd35a1da7009928
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-04-14 09:59:13 +0530

    update: homepage banner; ;

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,7 +2,17 @@ import Image from "next/image";
 
 const Banner = () => {
   return (
-      <Image width={700} height={250} className='rounded shadow-lg' src="/abhijayrajvansh-in-the-cyberworld.webp" alt="abhijay_as_curioustoddler_in_the_cyberworld" />
+      <>
+        <Image width={700} height={250} className=' sm:hidden rounded-2xl w-full shadow-lg' 
+          src="/images/abhijay-rajvansh-in-cyberworld-mobile.png" 
+          alt="abhijay_as_curioustoddler_in_the_cyberworld"
+        />
+
+        <Image width={700} height={250} className='hidden sm:block rounded-2xl w-full shadow-lg' 
+          src="/images/abhijay-rajvansh-in-cyberworld.png" 
+          alt="abhijay_as_curioustoddler_in_the_cyberworld"
+        />
+      </>
   )
 }
 
