5c2e1d76b3123b6f1b878e3f9a4a47781e705e3e
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-04-14 10:01:08 +0530

    update: funfact -> pitch

--- components/Awwwards.tsx
+++ components/Awwwards.tsx
@@ -26,20 +26,8 @@ const Awwwards = () => {
 
       </div>
 
-
-      <div className="flex flex-col sm:flex-row sm:space-x-7">
-
-        {/* <AchievementCard
-          title="Asia Largest Hackathon"
-          position="Winner, Smarthacks"
-          thumbnail="bg-[url('/fhc.webp')]"
-          tilt="left" 
-        /> */}
-       
-      </div>
-
       <div className="text-md mt-3 mb-5 font-light flex gap-1"> 
-          <span className="text-danger-500 font-semibold">funfact: <span className="text-default-900 font-light">I&apos;m good at <span className="text-default-900 font-medium">figuring things out</span>, that&apos;s my biggest flex!</span></span>
+          <span className="text-danger-500 font-semibold">pitch: <span className="text-default-900 font-light">I&apos;m good at <span className="text-default-900 font-medium">figuring things out</span>, that&apos;s my biggest flex!</span></span>
       </div>
 
     </div>
