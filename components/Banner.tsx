0f04c91ce6993ce71ea20edc257822b888d315eb
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-04-16 19:46:24 +0530

    fix: banner height

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Banner = () => {
   return (
       <>
-        <Image width={700} height={250} className=' sm:hidden rounded-2xl w-full shadow-lg' 
+        <Image width={700} height={226} className=' sm:hidden rounded-2xl w-full shadow-lg' 
           src="/images/abhijay-rajvansh-in-cyberworld-mobile.png" 
           alt="abhijay_as_curioustoddler_in_the_cyberworld"
         />
