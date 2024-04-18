08a4514c348f7f5262118fc84a43b35de18b9223
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-04-19 00:56:23 +0530

    fix: mobile banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,7 +6,6 @@ import LazyLoadImage from "./LazyloadImage";
 const Banner = () => {
   return (
       <>
-
         <Tooltip size={'sm'} 
           showArrow content={`AI generated myself in a cyberpunk world!`}
           placement="bottom" 
@@ -18,16 +17,15 @@ const Banner = () => {
             src="/images/abhijay-rajvansh-in-cyberworld.png" 
             alt="abhijay_as_curioustoddler_in_the_cyberworld"
           />
-
         </Tooltip>
 
         <LazyLoadImage 
-          blurredImageSrc="/images/ar-sign-no-bg.png" 
-          src="/images/abhijay-rajvansh-in-cyberworld-mobile.png" 
+          blurredImageSrc="/images/abhijay-rajvansh-in-cyberworld-mobile.jpg" 
+          src="/images/abhijay-rajvansh-in-cyberworld-mobile.jpg" 
           alt="ai_generated_abhijay_in_cyberworld" 
-          className='sm:hidden rounded-2xl w-full shadow-lg'
+          className='sm:hidden rounded-lg w-full shadow-lg'
         />
-        
+
       </>
   )
 }
