dacaf5e0eb7c8a0919f413a0c9721b8a2ee7cb52
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-04-21 21:52:00 +0530

    added: services thumbnail transition

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image';
 import React, { useState } from 'react';
 
 interface VideoComponentProps {
-  videosrc: string,
-  imgsrc: string,
-  title: string,
+  videosrc: string;
+  imgsrc: string;
+  title: string;
 }
 
 const VideoComponent: React.FC<VideoComponentProps> = ({ title, videosrc, imgsrc }) => {
@@ -19,37 +19,34 @@ const VideoComponent: React.FC<VideoComponentProps> = ({ title, videosrc, imgsrc
   };
 
   return (
-    <div 
-      onMouseEnter={handleMouseEnter} 
-      onMouseLeave={handleMouseLeave} 
-      className="relative w-full sm:w-[336px] h-48 mb-7">
-      
-      <h1 className='absolute w-full h-full text-3xl text-white font-bold rounded-xl flex items-center justify-center bg-black/50'>
+    <div
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className="relative w-full sm:w-[336px] h-48 mb-7 overflow-hidden"
+    >
+      <h1 className="absolute w-full h-full text-3xl text-white font-bold rounded-xl flex items-center justify-center bg-black/50">
         {title}
       </h1>
 
       <Image
-        width={300} 
-        height={176}  
-        src={imgsrc} 
-        alt='service-thumbnail' 
-        className='w-full h-full object-cover rounded-xl' 
+        width={300}
+        height={176}
+        src={imgsrc}
+        alt="service-thumbnail"
+        className="w-full h-full object-cover rounded-xl"
       />
-      
-      {isHovered && (
-        <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center text-white rounded-xl">
-          <video
-            src={videosrc}
-            autoPlay={isHovered}
-            loop
-            muted
-            playsInline
-            webkit-playsinline
-            className="w-full h-full object-cover rounded-xl"
-          />
-        </div>
-      )}
-    
+
+      <video
+        src={videosrc}
+        autoPlay
+        loop
+        muted
+        playsInline
+        webkit-playsinline
+        className={`absolute top-0 left-0 w-full h-full object-cover rounded-xl transition-opacity duration-500 ${
+          isHovered ? 'opacity-100' : 'opacity-0'
+        }`}
+      />
     </div>
   );
 };
