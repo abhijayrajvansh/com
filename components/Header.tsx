645f6b3553b06d8ebee761b2fe28e4dd5465dd1b
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-04-30 23:11:55 +0530

    header: quickLinks dropdown menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,47 +1,17 @@
 'use client';
 
 import SocialLinks from "./SocialLinks";
-import { Button, DropdownSection } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 import { Link } from "@nextui-org/react";
 import { RiBookMarkLine } from "react-icons/ri";
-import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from "@nextui-org/react";
-import { RxHamburgerMenu } from "react-icons/rx";
-import { useTheme } from "next-themes";
+import QuickLinks from "./QuickLinks";
 
 const Header = () => {
-  const { theme, setTheme } = useTheme();
-  
-  const handleTheme = () => {
-    theme === "light" ? setTheme("dark") : setTheme("light");
-  }
 
   return (
     <section className="flex items-center justify-between px-5 py-5 sm:pl-0 sm:ml-[110px] lg:ml-[230px]">
       
-      <div className="transition rounded-xl border border-background hover:border-blue-600 shadow-lg shadow-blue-600">
-        
-        <Dropdown backdrop="blur" >
-          <DropdownTrigger>
-            <Button variant="flat" color="primary" isIconOnly className="dark:text-white"> <RxHamburgerMenu size={20}/> </Button>
-          </DropdownTrigger>
-          
-          <DropdownMenu aria-label="Static Actions" >
-            
-            <DropdownSection title='Actions' showDivider >
-              <DropdownItem onClick={ handleTheme }>Go {theme === 'light' ? 'Dark' : 'Light'}</DropdownItem>    
-            </DropdownSection>
-
-            <DropdownSection title='Social Links'  >
-              <DropdownItem>X</DropdownItem>
-              <DropdownItem>Linkedin</DropdownItem>
-              <DropdownItem>Email</DropdownItem>
-            </DropdownSection>
-
-          </DropdownMenu>
-        
-        </Dropdown>
-
-      </div>
+      <QuickLinks />      
 
       <div className="flex items-center space-x-3 sm:space-x-10">
         <SocialLinks iconSize={21} showOnMobile={false}/>
---
+++ components/QuickLinks.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import { Button, Dropdown, DropdownTrigger, DropdownMenu, DropdownSection, DropdownItem } from "@nextui-org/react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { useTheme } from "next-themes";
+
+import { siteConfig } from "@/config/site";
+import { FaXTwitter, FaGithub, FaLinkedin } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import { PiSunLight } from "react-icons/pi";
+import { IoMoonOutline } from "react-icons/io5";
+
+const QuickLinks = () => {
+  
+  const { theme, setTheme } = useTheme();
+  
+  const handleTheme = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  }
+
+  return (
+    <div className="transition rounded-xl border border-background hover:border-blue-600 hover:border-blur">
+        
+        <Dropdown backdrop="blur" size="lg">
+          <DropdownTrigger>
+            <Button variant="flat" isIconOnly className="dark:text-white dark:bg-primary-100"> <RxHamburgerMenu size={20}/> </Button>
+          </DropdownTrigger>
+          
+          <DropdownMenu aria-label="Static Actions" >
+            
+            <DropdownSection title='Actions' showDivider >
+              <DropdownItem onClick={ handleTheme }>
+                <div className="flex items-center gap-5">
+                  { theme === 'dark' ? <PiSunLight size={20}/> : <IoMoonOutline size={20}/> }
+                  <p className="text-[16px]">Go {theme === 'light' ? 'Dark' : 'Light'}</p>
+                </div>
+              </DropdownItem>    
+            </DropdownSection>
+
+            <DropdownSection title='Social Links'>
+
+              <DropdownItem className="my-1">
+                <div className="flex items-center gap-5">
+                  <FaXTwitter size={20}/> 
+                  <p className="text-[16px]">Twitter</p>
+                </div>
+              </DropdownItem>
+
+              <DropdownItem className="my-1">
+                <div className="flex items-center gap-5">
+                  <FaGithub size={20}/> 
+                  <p className="text-[16px]">Github</p>
+                </div>
+              </DropdownItem>
+
+              <DropdownItem className="my-1">
+                <div className="flex items-center gap-5">
+                  <FaLinkedin size={20}/> 
+                  <p className="text-[16px]">LinkedIn</p>
+                </div>
+              </DropdownItem> 
+
+              <DropdownItem className="mt-1">
+                <div className="flex items-center gap-5">
+                  <MdEmail size={20}/> 
+                  <p className="text-[16px]">E-mail</p>
+                </div>
+              </DropdownItem> 
+            </DropdownSection>
+
+          </DropdownMenu>
+        
+        </Dropdown>
+
+      </div>
+  )
+}
+
+export default QuickLinks;
