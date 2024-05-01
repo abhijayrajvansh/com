ef79d1ff4a3df168df254cef485628390c707c36
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-05-01 06:32:15 +0530

    shortIntro: fixed -> color

--- components/ShortIntro.tsx
+++ components/ShortIntro.tsx
@@ -1,30 +1,45 @@
-import { title } from '@/components/primitives';
-import Link from 'next/link';
+import { title } from "@/components/primitives";
+import Link from "next/link";
 
 const ShortIntro = () => {
   return (
     <div className="space-y-5 font-medium">
-
       <p className="text-xl space-x-2 mt-10">
-        Hi, I&apos;m<span className={`font-medium text-primary-600`}>{" "}Abhijay Rajvansh</span>
+        Hi, I&apos;m
+        <span className={`font-medium text-primary-600`}>
+          {" "}
+          Abhijay Rajvansh
+        </span>
       </p>
 
-      <h1 className='text-4xl py-3 sm:text-5xl font-medium'>
+      <h1 className="text-4xl py-3 sm:text-5xl font-medium">
         <span>I develop </span>
-          <span className='
-            bg-gradient-to-r from-[#1cbc56] to-[#1cbc56] dark:from-[#6dd9c9] dark:to-[#68fd81] py-1 bg-cover bg-clip-text text-transparent font-semibold
-          '>full-stack</span>
-          <span> softwares that people <span className={title({color: 'pink'})}>love.</span>
+        <span
+          className="
+            bg-gradient-to-r from-[#1cbc56] to-[#1cbc56] dark:from-[#6dd9c9] dark:to-[#68fd81] bg-cover bg-clip-text text-transparent font-semibold
+          "
+        >
+          full-stack
+        </span>
+        <span>
+          {" "}
+          softwares that people{" "}
+          <span className="bg-gradient-to-b from-[#FD6BCE] to-[#F85798] bg-cover bg-clip-text text-transparent font-semibold">love.</span>
         </span>
       </h1>
 
       <p className="font-normal text-color-text leading-relaxed">
-        Freelance Web & Mobile Developer, Former Silicon Valley Intern, Apple Developer Awardee, Former AI/ML Researcher @L&D, UK and {' '}
-        <Link className="text-primary-600 font-medium sm:font-normal hover:underline underline-offset-4" href={'/about'}>more...</Link>
+        Freelance Web & Mobile Developer, Former Silicon Valley Intern, Apple
+        Developer Awardee, Former AI/ML Researcher @L&D, UK and{" "}
+        <Link
+          className="text-primary-600 font-medium sm:font-normal hover:underline underline-offset-4"
+          href={"/about"}
+        >
+          more...
+        </Link>
       </p>
-
     </div>
-  )
-}
+  );
+};
 
 export default ShortIntro;
