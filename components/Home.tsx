e26fa9c3190cb56592b11760b2d653efc8b9b3a0
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-05-16 14:47:52 +0530

    lighttheme: twitter, spotify, ui

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,59 +1,53 @@
-'use client';
+"use client";
 
 import Header from "./Header";
 import NavigationTab from "./NavigationTab";
 import RecentProjects from "./RecentProjects";
 import ShortIntro from "@/components/ShortIntro";
 import FeaturedArticles from "./FeaturedArticles";
-import Banner from "@/components/Banner"
+import Banner from "@/components/Banner";
 import Newsletter from "./Newsletter";
 import ThanksNote from "./ThanksNote";
-import Spotify from "./Spotify";
 import Twitter from "./Twitter";
 import TechnicalSkills from "./TechnicalSkills";
 import SocialConnections from "./SocialConnections";
 import Services from "./Services";
 
 const Home = () => {
+  return (
+    <>
+      <section className="wrapper">
+        <Header />
+        <NavigationTab />
 
-	return (
-			<>
-				<section className="wrapper">
-					<Header />
-					<NavigationTab />
+        <div className="pageWrapper">
+          <main className="contentWrapper">
+            <Banner />
 
-					<div className="pageWrapper">
-						<main className="contentWrapper">
+            <ShortIntro />
 
-							<Banner />
+            <FeaturedArticles />
 
-							<ShortIntro />
+            <Services />
 
-							<FeaturedArticles />
+            <RecentProjects />
+          </main>
 
-							<Services />
+          <section>
+            <Newsletter tilt="left" />
 
-							<RecentProjects />
-						</main>
-							
-						<section>
-							<Newsletter tilt="left" />
-							
-							<SocialConnections tilt="right" />
+            <SocialConnections tilt="right" />
 
-							<TechnicalSkills />
-							
-							<Twitter tilt="right"/>
-							
-							<Spotify tilt="left"/>
-							
-							<ThanksNote tilt="right" />
-						</section>
-						
-					</div>
-				</section>
-			</>
-	)
-}
+            <TechnicalSkills />
+
+            <Twitter tilt="right" />
+
+            <ThanksNote tilt="left" />
+          </section>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Home;
--- components/Twitter.tsx
+++ components/Twitter.tsx
@@ -4,43 +4,54 @@ import Image from "next/image";
 import avatar from "@/public/images/abhijayrajvansh-avatar.png";
 
 interface twitterProps {
-  tilt: string,
+  tilt: string;
 }
 
 const Twitter: React.FC<twitterProps> = ({ tilt }) => {
   return (
-    <div className={`ml-7 mb-7 shadow-xl rounded-xl mr-5 transition ${tilt === 'left' ? 'hover:-rotate-1' : 'hover:rotate-1'}  relative sm:ml-[116px] lg:ml-7 sm:w-[650px] lg:max-w-[300px] flex items-center justify-center border border-default-200/50 bg-gradient-to-b from-projectCardTop to-projectCardBotttom`}>
-    
-
-        <div className="w-full rounded-xl backdrop-blur-2xl flex flex-col items-start space-y-3 p-4 dark:bg-black/30 bg-black/90">
-
-         <div className="flex items-center w-full justify-between">
+    <div
+      className={`ml-7 mb-7 shadow-md rounded-xl mr-5 transition ${
+        tilt === "left" ? "hover:-rotate-1" : "hover:rotate-1"
+      }  relative sm:ml-[116px] lg:ml-7 sm:w-[650px] lg:max-w-[300px] flex items-center justify-center border border-default-200/50 bg-gradient-to-b from-projectCardTop to-projectCardBotttom`}
+    >
+      <div className="w-full rounded-xl backdrop-blur-2xl flex flex-col items-start space-y-3 p-4">
+        <div className="flex items-center w-full justify-between">
           <div className="flex items-center gap-3">
-            <Image 
-              className="rounded-full" 
-              width={40} height={40} 
+            <Image
+              className="rounded-full"
+              width={40}
+              height={40}
               src={avatar}
               alt="ar-avatar"
               placeholder="blur"
             />
 
             <div className="flex flex-col">
-              <p className="font-semibold text-sm text-white">Abhijay Rajvansh</p>
+              <p className="font-semibold text-sm ">Abhijay Rajvansh</p>
               <p className="text-color-text text-sm">@rajvanshtwt</p>
             </div>
           </div>
-          <Button as={Link} isExternal href={siteConfig.links.twitter} size="sm" className="bg-white text-black font-medium rounded-full px-3">Follow</Button>
-         </div>
+          <Button
+            as={Link}
+            isExternal
+            href={siteConfig.links.twitter}
+            size="sm"
+            className="dark:bg-white dark:text-black text-white bg-sky-500 font-medium rounded-full px-3"
+          >
+            Follow
+          </Button>
+        </div>
 
-         <p className="text-white text-base sm:text-sm">{tweets.post.tweet}</p>
+        <p className=" text-base sm:text-sm">{tweets.post.tweet}</p>
 
-         <div className="flex items-center justify-start text-sm text-color-text">
-          <p>{tweets.post.time} · {tweets.post.date}</p>
-         </div>
-
+        <div className="flex items-center justify-start text-sm text-color-text">
+          <p>
+            {tweets.post.time} · {tweets.post.date}
+          </p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Twitter;
