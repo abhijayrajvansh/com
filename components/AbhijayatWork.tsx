9e81c2eb779f688c280b46f24f112d50bd445be0
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-05-22 14:12:34 +0530

    update: next-ui -> shadcn/ui

--- components/AbhijayatWork.tsx
+++ components/AbhijayatWork.tsx
@@ -1,24 +1,16 @@
-import { Tooltip } from "@nextui-org/react";
 import Image from "next/image";
 
 const AbhijayatWork = () => {
   return (
-    <Tooltip
-      showArrow
-      content="one can envision me like this, lol :P"
-      placement="top-end"
-      color="secondary"
-    >
-      <Image
-        unoptimized
-        width={700}
-        height={200}
-        // max-h-[150px] sm:max-h-[200px] for some old banner
-        className="mb-5 max-h-[200px] sm:max-h-[350px] shadow-lg object-cover object-center rounded-lg border-2 border-default-200"
-        src="/abhijayatwork.gif"
-        alt="abhijay@work-pixelAnimated"
-      />
-    </Tooltip>
+    <Image
+      unoptimized
+      width={700}
+      height={200}
+      // max-h-[150px] sm:max-h-[200px] for some old banner
+      className="mb-5 max-h-[200px] sm:max-h-[350px] shadow-lg object-cover object-center rounded-lg border-2 border-default-200"
+      src="/abhijayatwork.gif"
+      alt="abhijay@work-pixelAnimated"
+    />
   );
 };
 
--- components/PreLoader.tsx
+++
@@ -1,7 +0,0 @@
-const PreLoader = () => {
-  return (
-    <div className="max-w-screen-xl h-screen bg-background flex items-center justify-center text-3xl font-light">loading...</div>
-  )
-}
-
-export default PreLoader;
