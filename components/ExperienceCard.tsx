30d2af472e71ca1f32aa258cfc1bb02f75956f66
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-05-22 17:54:46 +0530

    wagmi: minor bug fixes & improvements

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,48 +1,77 @@
 import Image from "next/image";
 
 interface ExperienceCardProps {
-  companyLogo: string,
-  startDate?: string,
-  endDate?: string,
-  position: string,
-  companyName: string,
-  companyLocation?: string,
-  description?: string[],
-  showTimeline?: boolean,
+  companyLogo: string;
+  startDate?: string;
+  endDate?: string;
+  position: string;
+  companyName: string;
+  companyLocation?: string;
+  description?: string[];
+  showTimeline?: boolean;
 }
 
-const ExperienceCard:React.FC<ExperienceCardProps> = ({companyLogo, startDate, endDate, position, companyName, companyLocation, description, showTimeline = true}) => {
+const ExperienceCard: React.FC<ExperienceCardProps> = ({
+  companyLogo,
+  startDate,
+  endDate,
+  position,
+  companyName,
+  companyLocation,
+  description,
+  showTimeline = true,
+}) => {
   return (
     <div className="flex gap-3 sm:gap-5">
+      {showTimeline && (
+        <div
+          className={`border-l border-zinc-500 relative left-10 sm:left-12`}
+        ></div>
+      )}
+      {!showTimeline && (
+        <div
+          className={`border-l border-background relative left-10 sm:left-12`}
+        ></div>
+      )}
 
-      {showTimeline && <div className={`border-l border-default-400 relative left-10 sm:left-12`}></div>}
-      {!showTimeline && <div className={`border-l border-background relative left-10 sm:left-12`}></div>}
-      
       <div className="min-w-[56px] z-10">
-        <Image height={56} width={56} 
-          className={`h-14 rounded-xl ${endDate === 'Present' ? 'border-3 border-yellow-400 border-blur-yellow' : 'border-3 border-default-300'}`} src={companyLogo} alt="companyLogo" />
+        <Image
+          height={56}
+          width={56}
+          className={`h-14 rounded-xl ${
+            endDate === "Present"
+              ? "border-2 border-yellow-400 border-blur-yellow"
+              : "border-2 border-zinc-500"
+          }`}
+          src={companyLogo}
+          alt="companyLogo"
+        />
       </div>
-      
+
       <div className="flex flex-col gap-1 pl-1 pb-7 sm:pb-10">
-        {startDate && <p className="text-default-500 text-sm">{startDate} - {endDate}</p>}
+        {startDate && (
+          <p className="text-default-500 text-sm">
+            {startDate} - {endDate}
+          </p>
+        )}
         <h2 className="text-xl sm:text-2xl font-semibold">{position}</h2>
         <div className="flex items-center gap-2 pb-1">
           <div className="text-sm font-medium text-default-600">
             <h3 className="">{companyName}</h3>
-            <p className="text-default-500">{companyLocation}</p>
+            <p className="text-color-text">{companyLocation}</p>
           </div>
         </div>
-          {
-            description?.map((keyPoint, index) => (
-              <div className="text-default-500 text-[13px] sm:text-sm" key={index}>
-                  <p className="pt-1"><span className="text-primary-600"> — </span>{keyPoint}</p>
-              </div>
-            ))
-          }
+        {description?.map((keyPoint, index) => (
+          <div className="text-color-text text-[13px] sm:text-sm" key={index}>
+            <p className="pt-1">
+              <span className="text-primary-600"> — </span>
+              {keyPoint}
+            </p>
+          </div>
+        ))}
       </div>
-    
     </div>
-  )
-}
+  );
+};
 
 export default ExperienceCard;
--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -17,12 +17,12 @@ const SocialLinks = ({ iconSize, showOnMobile = false }:SocialLinksProps) => {
 
   return (
     <div className={`${!showOnMobile && 'hidden'} flex space-x-5`}>
-      <Link color="foreground" href={siteConfig.links.twitter}><FaXTwitter  size={iconSize}  className="hover:scale-125 transition"/></Link>
-      <Link color="foreground" href={siteConfig.links.github}><FaGithub  size={iconSize}  className="hover:scale-125 transition"/></Link>
-      <Link color="foreground" href={siteConfig.links.linkedin}><FaLinkedin  size={iconSize}  className="hover:scale-125 transition"/></Link>
-      <Link color="foreground" href={siteConfig.links.instagram}><FaInstagram  size={iconSize}  className="hover:scale-125 transition"/></Link>
-      <Link color="foreground" href={siteConfig.links.youtube}><FaYoutube  size={iconSize}  className="scale-125 hover:scale-150 transition"/></Link>
-      <Link color="foreground" href={siteConfig.links.email}><MdEmail  size={iconSize}  className="scale-125 hover:scale-150 transition"/></Link>
+      <Link target="_blank" rel="noopener noreferer" color="foreground" href={siteConfig.links.twitter}><FaXTwitter  size={iconSize}  className="hover:scale-125 transition"/></Link>
+      <Link target="_blank" rel="noopener noreferer" color="foreground" href={siteConfig.links.github}><FaGithub  size={iconSize}  className="hover:scale-125 transition"/></Link>
+      <Link target="_blank" rel="noopener noreferer" color="foreground" href={siteConfig.links.linkedin}><FaLinkedin  size={iconSize}  className="hover:scale-125 transition"/></Link>
+      <Link target="_blank" rel="noopener noreferer" color="foreground" href={siteConfig.links.instagram}><FaInstagram  size={iconSize}  className="hover:scale-125 transition"/></Link>
+      <Link target="_blank" rel="noopener noreferer" color="foreground" href={siteConfig.links.youtube}><FaYoutube  size={iconSize}  className="scale-125 hover:scale-150 transition"/></Link>
+      <Link target="_blank" rel="noopener noreferer" color="foreground" href={siteConfig.links.email}><MdEmail  size={iconSize}  className="scale-125 hover:scale-150 transition"/></Link>
     </div>
   )
 }
