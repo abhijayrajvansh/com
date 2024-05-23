f79d62b12dfef0a600806fef83af8bdc3d20ffb4
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-05-23 19:28:41 +0530

    wagmi: minor bug fixes & improvements

--- components/Notfound.tsx
+++ components/Notfound.tsx
@@ -1,21 +1,43 @@
+import Link from "next/link";
 import Header from "./Header";
 import NavigationTab from "./NavigationTab";
+import { siteConfig } from "@/config/site";
 
 const Notfound = () => {
   return (
     <section className="wrapper">
-        <Header />
-        <NavigationTab />
-        <div className="pageWrapper">
-          <main className="contentWrapper">
-            <div className="flex flex-col items-center justify-center sm:pt-20">
-              <p className="text-2xl font-light">404 | not-found.</p>
-              <p className="text-sm">there&apos;s nothing here, go back :P</p>
+      <Header />
+      <NavigationTab />
+      <div className="pageWrapper">
+        <main className="contentWrapper">
+          <div className="flex flex-col items-start justify-center sm:pt-20">
+            <p className="py-1 sm:text-xl font-light">
+              hey, looks like this page doesn&apos;t exist...
+            </p>
+            <p className="py-1 sm:text-xl font-light">
+            think it&apos;s a mistake? tell me via{" "}
+              <Link
+                className="primary-text hover:underline underline-offset-4"
+                href={siteConfig.links.email}
+              >
+                email
+              </Link>{" "}
+              or DM on{" "}
+              <Link
+                className="primary-text hover:underline underline-offset-4"
+                href={siteConfig.links.twitter}
+              >𝕏.
+              </Link>
+            </p>
+            <br />
+            <div className="font-light">
+              <p className="sm:text-lg">regards</p>
+              <p className="sm:text-lg">abhijay</p>
             </div>
-            
-          </main>
-        </div>
-      </section>
+          </div>
+        </main>
+      </div>
+    </section>
   );
 };
 
