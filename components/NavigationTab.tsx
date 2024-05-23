431d0dfa416b80855fea192b9c7b7517fe2324d6
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-05-23 19:50:31 +0530

    wagmi: minor bug fixes & improvements

--- components/NavigationTab.tsx
+++ components/NavigationTab.tsx
@@ -11,7 +11,6 @@ import Link from "next/link";
 import Image from "next/image";
 import avatar from "@/public/images/abhijayrajvansh-avatar.png";
 
-
 const NAVIGATION_DATA = [
   {
     pageTitle: "Home",
@@ -46,7 +45,7 @@ const NavigationTab = () => {
     <header className="bg-background/50 z-20 backdrop-blur-[5px] bg-opacity-60 fixed max-w-screen-xl w-full bottom-0 sm:w-fit sm:px-2 lg:pr-4 sm:top-0 sm:pt-0 sm:border-r border-border sm:font-light">
       <div className="flex items-center justify-evenly w-full sm:block sm:pt-5 sm:mt-2">
         <Link href={"/"} className="hidden sm:flex mb-5 justify-center">
-          <div className="flex items-center gap-3">
+          <div className="flex items-center space-x-2 pl-2">
             <Image
               width={40}
               height={40}
