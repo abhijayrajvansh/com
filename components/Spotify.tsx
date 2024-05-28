30a878087404a6b6a590b9318d9b1847ae8cb3da
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-05-28 18:44:13 +0530

    fix: access to image domains

--- components/Spotify.tsx
+++ components/Spotify.tsx
@@ -1,6 +1,7 @@
 import { FaSpotify } from "react-icons/fa6";
 import { FaCircle } from "react-icons/fa";
 import { spotifyData } from "@/config/spotify.config";
+import Image from "next/image";
 
 const Spotify = () => {
   return (
@@ -8,7 +9,7 @@ const Spotify = () => {
       <div
         className={`relative rounded-xl font-medium text-lg sm:mt-0 mt-5 sm:ml-[90px] lg:ml-0 sm:w-[650px] lg:max-w-[300px] border border-border mb-5 shadow-md transition bg-black/30`}
       >
-        <img
+        <Image height={500} width={500}
           className="w-full h-full rounded-xl absolute object-cover -z-10"
           src={spotifyData.bannerImgUri}
           alt="spotify-track-banner.png"
--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,12 @@ const nextConfig = {
                 port: '',
                 pathname: '/**'
             },
+            {
+                protocol: 'https',
+                hostname: 'c.saavncdn.com',
+                port: '',
+                pathname: '/**'
+            },
         ],
     },
 };
