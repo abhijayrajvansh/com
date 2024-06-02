0ea367ed0ca1adacf1568992b5986bbe98771fa2
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-06-02 08:11:27 +0530

    removed: services section

--- components/Home.tsx
+++ components/Home.tsx
@@ -26,7 +26,7 @@ const Home = () => {
             <Banner />
             <Bio />
             <FeaturedArticles />
-            <Services />
+            {/* <Services /> */}
             <RecentProjects />
           </main>
 
