a9f70f6082ae6aad408d1af18615c6fc0ac3902c
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-06-03 20:18:03 +0530

    wagmi: fixed bugs & improvements

--- components/Spotify.tsx
+++ components/Spotify.tsx
@@ -6,7 +6,7 @@ const Spotify = () => {
   return (
     <div className="w-full px-5 sm:ml-2 rounded-xl ">
       <div
-        className={`relative rounded-xl font-medium text-lg sm:mt-0 mt-5 sm:ml-[90px] lg:ml-0 sm:w-[650px] lg:max-w-[300px] border border-border mb-5 shadow-md transition bg-black/50`}
+        className={`relative rounded-xl font-medium text-lg sm:mt-0 mt-5 sm:ml-[90px] lg:ml-0 sm:w-[650px] lg:max-w-[300px] border border-border mb-5 shadow-md transition dark:bg-black/50 bg-black/30`}
       >
         <Image
           height={500}
