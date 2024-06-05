70da1bdc9462942a6bfafe049ec5cf2f07849ceb
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-06-05 22:44:47 +0530

    added glow-bg css

--- config/project.config.ts
+++ config/project.config.ts
@@ -9,7 +9,7 @@ export const allProjects = [
     "popular":true,
     "banner": '/projects/fullstack-twitter.webp',
     "logo": '/logo/xLogo.jpeg',
-    "title": "Twitter 𝕏 Fullstack Clone",
+    "title": "Twitter 𝕏 Clone",
     "descriptiom": "this is a sample description of the project card after open the model of the card, good going!!.",
     "sourceCode": "https://github.com/abhijayrajvansh/twitter-clone-next.js",
     "demo": "https://github.com/abhijayrajvansh/twitter-clone-next.js",
