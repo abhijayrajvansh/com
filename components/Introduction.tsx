535955baa14740f508775f0f67c1ecc147b757dc
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-06-07 02:57:09 +0530

    update: about magazine layout

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -1,69 +1,68 @@
 import Link from "next/link";
 import DigitalHandSign from "./DigitalHandSign";
 import CalltoAction from "./CalltoAction";
-import NextParagraph from "./NextParagraph";
+import Image from "next/image";
 
 const Introduction = () => {
   return (
     <div>
       <h1 className="heading pink-text-bg-gradient">Who, What & Why?</h1>
 
-      {/* <video
-          className="w-full sm:h-[282.326px] rounded-md mb-5"
-          src="/videos/abhijayWalkingInCyberworld.mp4"
-          autoPlay loop playsInline webkit-playsinline="true" 
-        /> */}
+      <div className="">
+        <img
+          src="/images/abhijay-rajvansh.jpeg"
+          alt="abhijay-rajvansh-avatar.png"
+          className="flex-none h-[230px] mr-5 rounded-lg float-left"
+        />
 
-      <div>
-        <p className="text-color-text font-light">
-          In the fast-paced tech world, keeping up is tough. Product Hunt and
-          HubSpot launch features daily, and AI is everywhere. Standing still
-          means falling behind.
-          <NextParagraph />
-          That&apos;s where I come in. With over
-          <span className="text-highlighted"> 4+ years of experience </span>
-          as a freelance software engineer and technical consultant, I help turn
-          ideas into fully functional digital products. I offer practical
-          solutions and bridge the tech gap for non-tech individuals.
-          <NextParagraph />
-          For me,
-          <span className="text-highlighted ">
-            {" "}
-            software engineering is an art{" "}
-          </span>
-          , akin to Da Vinci&apos;s painting. I prioritize researching new
-          methodologies to develop{" "}
-          <Link
-            href={"/projects"}
-            className="primary-text font-medium hover:underline underline-offset-4"
-          >
-            optimized solutions
-          </Link>
-          , rather than simply replicating code. I&apos;m passionate about
-          <span className=" "> algorithms & UI/UX </span>
-          design that&apos;s why I love educating fellow developers about my
-          unique approaches to solving problems through videos & writing{" "}
-          <Link
-            href={"/blog"}
-            className="primary-text font-medium hover:underline underline-offset-4"
-          >
-            blogs
-          </Link>{" "}
-          in my free time.
-          <NextParagraph />
-          Apart from coding, I enjoy playing my piano, reading about finance and
-          psychology, watching arthouse movies, and sometimes playing chess.
-          <NextParagraph />
-          
-          <span className="text-highlighted">
+        <div className="">
+          <p className="text-paragraph">
+            The rapidly evolving tech industry makes staying updated
+            challenging. Product Hunt and HubSpot release new features daily,
+            and AI is ubiquitous. Standing still means falling behind.
+          </p>
+
+          <p className="text-paragraph mt-3">
+            That&apos;s where I come in. With over
+            <span className="text-highlighted"> 4+ years of experience </span>
+            as a freelance software engineer and technical consultant, I help
+            turn ideas into fully functional digital products. I offer practical
+            solutions and bridge the tech gap for non-tech individuals.
+          </p>
+
+          <p className="text-paragraph mt-3">
+            For me,{" "}
+            <span className="text-highlighted">
+              {" "}
+              software engineering is an art
+            </span>
+            , akin to Da Vinci&apos;s painting. I prioritize researching new
+            methodologies to develop{" "}
+            <Link href={"/projects"} className="text-link">
+              optimized solutions
+            </Link>
+            , rather than simply replicating code. I&apos;m passionate about
+            algorithms & UI/UX design that&apos;s why I love educating fellow
+            developers about my unique approaches to solving problems through
+            videos & writing{" "}
+            <Link href={"/blog"} className="text-link">
+              blogs
+            </Link>{" "}
+            in my free time.
+          </p>
+          <p className="text-paragraph mt-3">
+            Apart from coding, I enjoy playing my piano, reading about finance
+            and psychology, watching arthouse movies, and sometimes playing
+            chess.
+          </p>
+          <p className="text-highlighted mt-3">
             tl;dr: i&apos;m an artist who aims for his art to leave behind a
             lasting legacy!
-          </span>
-
-        </p>
-
-        <DigitalHandSign />
+          </p>
+        </div>
       </div>
+
+      <DigitalHandSign />
 
       <div className="my-10">
         <h1 className="text-3xl font-semibold my-2 yellow-text-bg-gradient">
