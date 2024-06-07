69636fb88b4e85bd534260afad1eacb37f4a7654
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-06-08 01:30:23 +0530

    update: not-found page

--- components/Notfound.tsx
+++ components/Notfound.tsx
@@ -14,13 +14,12 @@ const Notfound = () => {
       <div className="pageWrapper">
         <main className="contentWrapper">
           <div className="flex flex-col text-base font-light items-start justify-center sm:pt-20">
-            <p className="text-5xl font-bold">
-              404 error &nbsp; : P
+            <p className="text-5xl font-bold">404 error</p>
+            <p className="mt-5">
+              hey, looks like this page doesn&apos;t exist...
             </p>
-            <p className="mt-5">hmm, looks like this page doesn&apos;t exist...</p>
-            <p>
-              believe it&apos;s a mistake or a bug? you can cantact me
-              via{" "}
+            <p className="mt-2">
+              believe it&apos;s a mistake or a bug? you can cantact me via{" "}
               <Link
                 className="primary-text hover:underline underline-offset-4"
                 href={siteConfig.links.email}
@@ -34,11 +33,11 @@ const Notfound = () => {
                 className="primary-text hover:underline underline-offset-4"
                 href={siteConfig.links.twitter}
               >
-                𝕏(twitter).
+                𝕏 (twitter).
               </Link>
             </p>
             <p className="mt-3">regards</p>
-            <p>abhijay ; )</p>
+            <p>abhijay ;)</p>
           </div>
         </main>
         <section>
