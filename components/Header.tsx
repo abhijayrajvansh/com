bb9d0f9b56e6802172b73e6a1202d8092f3f292c
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-06-13 10:39:15 +0530

    update: header ui

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import { ModeToggle } from "./ModeToggle";
 const Header = () => {
 
   return (
-    <section className="flex items-center justify-between py-5 px-4 xl:px-0 sm:pl-0 sm:ml-[110px] xl:ml-[230px] max-w-[1030px]">
+    <section className="flex items-center justify-between py-5 px-4 xl:px-0 sm:ml-[110px] xl:ml-[230px] xl:w-[1030px]">
       <ModeToggle />
       <GuestbookBtn />
     </section>
