ca3c417f1f1e49da0d06dd315b37a4b46985226e
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-08-09 01:21:14 +0530

    update: spotify song

--- config/spotify.config.ts
+++ config/spotify.config.ts
@@ -1,5 +1,5 @@
 export const spotifyData = {
-  songName: "Alibi (with Pabllo Vittar &amp; Yseult)",
+  songName: "Alibi",
   artist: "Sevdaliza",
   bannerImgUri: "https://c.saavncdn.com/256/Energia-Parte-2--English-2018-20180828004641-500x500.jpg",
   spotifyLink: "https://open.spotify.com/track/5496lks6V3fh1mZAB6IAap"
