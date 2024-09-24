79e4cb1b178c6faa0a7ddf2d1cbace85e69fce1d
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-09-25 03:54:20 +0530

    todo: create api connection 4 spotify & yt

--- components/Spotify.tsx
+++ components/Spotify.tsx
@@ -1,81 +1,53 @@
 "use client";
 
-import { FaSpotify } from "react-icons/fa";
-import Image from "next/image";
-import { config, SPOTIFY_CONFIG_URL } from "@/config/spotify.config";
+import { SPOTIFY_CONFIG_URL } from "@/config/spotify.config";
 import { useEffect, useState } from "react";
 
 const Spotify = () => {
-
   const [songId, setSongId] = useState<String | null>(null);
+  const [videoId, setVideoId] = useState<String | null>(null);
 
   useEffect(() => {
     const fetchSongData = async () => {
       try {
-        const response = await fetch(
-          `${SPOTIFY_CONFIG_URL}/songId.json`
-        );
+        const response = await fetch(`${SPOTIFY_CONFIG_URL}/songId.json`);
 
         if (!response.ok) {
-          throw new Error('Failed to fetch the song data');
+          throw new Error("Failed to fetch the song data");
         }
 
         const jsonData = await response.json();
-        setSongId(jsonData.songId);
+        // setSongId(jsonData.songId);
+        setSongId("0yNiaePZow0ycdrmLV0J7y");
+        setVideoId("HZsiToZ8zCI");
       } catch (err: any) {
-        console.log(err.message)
+        console.log(err.message);
       }
     };
 
     fetchSongData();
-  }, []); // Runs once when the component mounts
+  }, []);
 
   return (
-    <div className="w-full px-5 sm:ml-2 mb-5">
-      <div
-        className={`relative rounded-t-xl font-medium text-lg sm:mt-0 mt-5 sm:ml-[90px] lg:ml-0 sm:w-[650px] lg:max-w-[300px] border border-border shadow-md transition dark:bg-black/40 bg-black/30 `}
-      >
-        <Image
-          className="w-full h-full rounded-t-xl absolute object-cover object-center -z-10"
-          height={300}
-          width={300}
-          src={config.cover}
-          alt="spotify-thumbnail.jpg"
-        />
-
-        <video
-          className="w-full h-full rounded-t-xl absolute object-cover object-center -z-10"
-          autoPlay={true}
-          loop={true}
-          playsInline={true}
-          muted={true}
-          src={config.preview}
-        ></video>
-
-        <div className="z-10 flex flex-col justify-between p-4 rounded-xl space-y-5 min-h-52">
-          <div className="flex items-center justify-between">
-            <FaSpotify
-              className="text-green-500 bg-black rounded-full"
-              size={37}
-            />
-            <Image
-              unoptimized
-              height={36}
-              width={36}
-              src="/images/audio-wave.gif"
-              alt="audio-wave-animationa"
-            />
-          </div>
-          <div className="space-y-5"></div>
-        </div>
-      </div>
-      <iframe
-        className="rounded-2xl sm:ml-[90px] sm:w-[648px] lg:w-[300px] lg:ml-[0px] relative -top-3"
+    <div className="w-full px-5 sm:ml-2 my-5 space-y-5">
+      <iframe // spotify
+        className="rounded-2xl sm:ml-[90px] sm:w-[648px] lg:w-[300px] lg:ml-[0px]"
         src={`https://open.spotify.com/embed/track/${songId}?utm_source=generator`}
-        height="80"
+        height="152"
         width="100%"
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
       ></iframe>
+
+      <div
+        className={`relative rounded-xl font-medium text-lg sm:mt-0 mt-5 sm:ml-[90px] lg:ml-0 sm:w-[650px] lg:max-w-[300px] border border-border shadow-md transition bg-black/10 min-h-44`}
+      >
+        <iframe // youtube
+          className="w-full h-full rounded-xl absolute object-cover object-center -z-10"
+          src={`
+          https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&controls=0&loop=1&playlist=${videoId}&rel=0&fs=0`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        ></iframe>
+      </div>
     </div>
   );
 };
